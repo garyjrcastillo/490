cf83aecf7adda5398716e8eedee80279a0c9182e
garyjrcastillo <garyjrcastillo@gmail.com>
2018-10-27 17:14:09 -0400

    updated with if statements so that code is properly executed depending on what session is set

--- frontend/testRabbitMQClient.php
+++ frontend/testRabbitMQClient.php
@@ -3,10 +3,11 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-session_start();
 
 #include("index.php");
 
+
+session_start();
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
@@ -17,34 +18,123 @@ else
   $msg = "test message";
 }
 
+
+
+if(isset($_SESSION["index"])){
 $request = array();
-$request['type'] = "login";
-$request['username'] = $_POST['username'];
-$request['password'] = $_POST['password'];
-#$request['username'] = "test";
-#$request['password'] = "test";
-$request['message'] = $msg;
-$response = $client->send_request($request);
-//$response = $client->publish($request);
 
-echo "client received response: ".PHP_EOL;
-print_r($response);
+//if(isset($_SESSION["index"])){
+	$request['type'] = "login";
+	$request['username'] = $_POST['username'];
+	$request['password'] = $_POST['password'];
+	
+	
+//	$request['username'] = 'test';
+	
+//	$request['password'] = 'test';
+	
+	$request['message'] = $msg;
+	$response = $client->send_request($request);
 
-if ($response == 1){
-	echo("<br>RESPONSE WORKED, Authentication Successfull<br>");
-	header("Location: welcome.php");
+	echo "client received response: ".PHP_EOL;
+	print_r($response);
+
+	if ($response==1){
+		echo("RESPONSE WORKED");
+		$_SESSION["sessionId"] = 'random';
+		header("Location: welcome.php");
+	}
+	else{
+		echo("WRONG RESPONSE");
+		$_SESSION["wrong"] = 'true';
+		header("Location: index.php");
+	}
+//}
+
 }
-else{
-	echo("WRONG RESPONSE, Authentication Failure");
-	$_SESSION['wrong'] = 'true';
-	header("Location: index.php");
-	echo ("WRONG");
+
+if(isset($_SESSION["register"])){
+	$request = array();
+	$request['type'] = "register";
+
+	$request['username'] = $_POST['username'];
+
+	$request['password'] = $_POST['password'];
+
+	$request['message'] = $msg;
+	$response = $client->send_request($request);
+	
+	echo "client received response: ".PHP_EOL;
+	
+	
+	print_r($response);
+
+	if ($response ==1){
+		header("Location: test.php");
+	}
 }
 
-#echo "\n$msg";
+
+if(isset($_SESSION["validate"])){
+	$request = array();
+	$request['type'] = "validate_session";
+	$request['sessionId'] = $_SESSION["sessionId"] ;
+
+	$response = $client->send_request($request);
+	echo "client received response: ".PHP_EOL;
+	print_r($response);
+
+	if ($response !=1)
+	{
+		header("Location: index.php");
+	}
+}
+
+//if(isset($_SESSION["index"])){
+
+	
+
+//	$response = $client->send_request($request);
+	
+//	echo "client received response: ".PHP_EOL;
+	
+	
+//	print_r($response);
+	
+//	if ($response == 1){
+//		echo("<br>RESPONSE WORKED, Authentication Successfull<br>");
+//		header("Location: welcome.php");
+	
+//	}
+//	else{
+//		echo("WRONG RESPONSE, Authentication Failure");
+//		$_SESSION["wrong"] = 'true';
+//		header("Location: index.php");
+//	}
+
+//}
+
+
+//if(isset($_SESSION["register"])){
+	
+//	$response = $client->send_request($request);
+
+//	echo "client received response: ".PHP_EOL;
+
+//	print_r($response);
+	
+//	if ($response ==1){
+//		header("Location: test.php");
+//	}
+//	else
+//	{
+//		header("Location: index.php");
+//	}
+
+//}
+
 
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
 
-#header("Location: welucome.php");
