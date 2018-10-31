9567573a2c04a8f2649716931ea51ccb40912804
garyjrcastillo <garyjrcastillo@gmail.com>
2018-10-31 18:35:08 -0400

    mysql error log

--- backend/config.php
+++ backend/config.php
@@ -3,16 +3,18 @@ $hostname ="localhost";
 $usernameSQL="gary";
 $passwordSQL="gary";
 $database="users";
-error_reporting(E_ERROR | E_PARSE | E_NOTICE);
-ini_set('displayerrors', 1);
+
 $db = mysqli_connect($hostname,$usernameSQL,$passwordSQL,$database);
 if (mysqli_connect_errno())
 {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo ("\n"."Failed to connect to MySQL: " . mysqli_connect_error() . "\n" );
+	error_log(mysqli_connect_error(),3,"/home/gary/490/backend/log2.txt");
         exit();
 }
-print"<br>Succesfully connected to MySQL.<br>";
+print("\n"."Succesfully connected to MySQL."."\n");
 mysqli_select_db($db,$database);
+
 #mysqli_close($db);
 #exit("<br> Interaction completed.<br><br>"); #this will not let the html come up.
+
 ?>
