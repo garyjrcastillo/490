b72dfda4fddaaee116a0704d0a594cffda123c14
garyjrcastillo <43897914+garyjrcastillo@users.noreply.github.com>
2018-12-18 07:43:42 -0500

    Updated to have session ids and api go through rabbitmq
    Now when a user logs in they will get a random sessionID put into their row on a table called loginform(which has the list of all users login and pass)
    Now we have a function called doApi which allows the listener to see what and when apis are called when testRabbitMQServer.php is being run.

--- backend/testRabbitMQServer.php
+++ backend/testRabbitMQServer.php
@@ -5,18 +5,23 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 //
+#session_start(); #
 function doLogin($username,$password)
 {
 	include('config.php');
-	
+	$_SESSION["uname"] = $username; #
+
 
 	$query = "SELECT * FROM loginform WHERE username = '$username' and password= '$password' ";
-	print("\n"."SQL Select statement is: $query"."\n") ;
+#	print("\n"."SQL Select statement is: $query"."\n") ;
        
 
-	if(!mysqli_query($db,$query)){
-	       	error_log("\n". mysqli_error($db)." \n" ,3, "/home/gary/490/backend/log2.txt");
-	}
+	($t = mysqli_query($db,$query) ) or die(mysqli_error($db) );
+
+#	   if(!mysqli_query($db,$query)){
+#                error_log("\n". mysqli_error($db)." \n" ,3, "/home/gary/490/backend/log2.txt");
+#        }
+
 
 
 	$count = mysqli_num_rows(mysqli_query($db,$query));
@@ -26,6 +31,15 @@ function doLogin($username,$password)
 	//IF THE USER / PASS ARE CORRECT
 	if ($count==1){
 		echo "\n"."Login Successfull". "\n";
+
+		$random = substr(str_shuffle(MD5(microtime())),0,8);
+
+		$setID = "UPDATE loginform SET sessionID = '$random' WHERE username = '$username' ";
+
+		($g = mysqli_query($db,$setID) ) or die(mysqli_error($db) );
+
+
+		$_SESSION["setID"] = $random;	
 		return true;
 			
 	}
@@ -43,29 +57,41 @@ function doLogin($username,$password)
 
 function doRegister($username, $password)
 {
+	
 		
 	include('config.php');
 
 	$s = "insert into loginform (username, password)  values ('$username','$password' ) ";
-	print "\n"."SQL Insert into statement is $s"."\n";
+#	print "\n"."SQL Insert into statement is $s"."\n";
 
 	($t = mysqli_query($db,$s) ) or die(mysqli_error($db) );
 
-	print "\n". "SQL Insert into loginform statement was transmitted for execution."."\n";
 
 
-	if( $t=mysqli_query($db,$s) ){
+#	print "\n". "SQL Insert into loginform statement was transmitted for execution."."\n";
+
+
+
+#	$query = "insert into foodHistory WHERE username = '$username' ";#
+#        ($x = mysqli_query($db,$query) ) or die(mysqli_error($db) );
+
+
+	if( $t == true ){
+		echo "Registration Successful";
 		return true;
 	}
 	else{
+		echo "Registration Failure";
 		return false;
 	}
 }
 
 function doValidate($sessionId){
-	if ($sessionId == 'random')
+	#if ($sessionId == 'random')
+	if ($sessionId == $_SESSION["setID"] )
 	{
 		echo "\n". "Session validated". "\n";
+		#echo "username is" . $_SESSION["uname"] ; #
 		return true;
 	}	
 	else{
@@ -73,6 +99,18 @@ function doValidate($sessionId){
 		return false;
 	}
 }
+
+#function doLogs($test){
+function doLogs(){
+	echo "TESTING \n";
+	return true;	
+}
+
+function doAPI($name){
+        #echo "$name \n";
+        return true;
+}
+
 
 function requestProcessor($request)
 {
@@ -94,6 +132,12 @@ function requestProcessor($request)
 
     case "validate_session":
       return doValidate($request['sessionId']);
+    case "get_logs":
+	    return doLogs();
+    case "get_api":
+        return doAPI($request['name']);
+
+      #return doLogs($request['test']);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
