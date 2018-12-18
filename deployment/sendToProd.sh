4f4d0d1b66403ec7423b010bdc1e18c072982f71
garyjrcastillo <43897914+garyjrcastillo@users.noreply.github.com>
2018-12-18 07:54:09 -0500

    add sendToProd.sh
    Allows deployment to iterate through a file called versionNumber.txt (which stores the number of current version folder we are at) 
    It lets a variable equal to the number in this text file
    Now that we have the correct version number in a variable we send the file inside the version folder to Production

---
+++ deployment/sendToProd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+                
+#echo 'What version do you want to roll back to?'
+
+#read version
+
+#this will read our text file that is maintaining the current version folder we are in
+
+b=1
+while read p;do
+        echo "$p"
+	let b=$p
+done <versionNumber.txt
+
+#by default b is 1, after the while loop it will roll back to the second last version
+echo "$b"
+
+
+scp ~/version$b/* yik@192.168.2.3:/var/www/html
