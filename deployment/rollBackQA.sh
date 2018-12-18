29f576e8cb3e513722f87256a497f95a8699571a
garyjrcastillo <43897914+garyjrcastillo@users.noreply.github.com>
2018-12-18 08:00:13 -0500

    add rollBackQA.sh
    Allows deployment to iterate through a file called versionNumber.txt ( which stores the number of the current version folder we are at)
    We then let a variable be equal to 1 less than the number stored in the text file.
    This way we now have a variable that is one less the current version folder we are at.
    Now by using the variable, we can send the file in the version folder that is one less than the current version folder we are at.
    Now we send to it QA, essentially rolling back to a previous version.

---
+++ deployment/rollBackQA.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+                
+#echo 'What version do you want to roll back to?'
+
+#read version
+
+#this will read our text file that is maintaining the current version folder we are in
+
+b=1
+while read p;do
+        echo "$p"
+	let b=$p-1
+done <versionNumber.txt
+
+#by default b is 1, after the while loop it will roll back to the second last version
+echo "$b"
+
+
+scp ~/version$b/mainpage.php nithin@192.168.2.7:/var/www/html
+
